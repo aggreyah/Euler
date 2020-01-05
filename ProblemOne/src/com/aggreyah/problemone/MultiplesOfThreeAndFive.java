1820c39bc1f32fe8a29e0a66c78adc23439e5630
aggreyah <aggreyah@gmail.com>
2020-01-05 15:04:16 +0300

    deleted unnecessary code from problemone

--- ProblemOne/src/com/aggreyah/problemone/MultiplesOfThreeAndFive.java
+++ ProblemOne/src/com/aggreyah/problemone/MultiplesOfThreeAndFive.java
@@ -13,10 +13,9 @@ public class MultiplesOfThreeAndFive {
 	}
 
 	public static void main(String[] args) {
-		MultiplesOfThreeAndFive myObject0 = new MultiplesOfThreeAndFive();
-		MultiplesOfThreeAndFive myObject1 = new MultiplesOfThreeAndFive(1000);
+		MultiplesOfThreeAndFive myObject = new MultiplesOfThreeAndFive(1000);
 		int sum = 0;
-		for (int i = 1; i < myObject1.value; i++) {
+		for (int i = 1; i < myObject.value; i++) {
 			if ((i % 3) == 0 || (i % 5) == 0 )
 				sum += i;
 		}
